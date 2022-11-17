db94f2d748d8598ad9c86dbb0a89c56f6eb42472
ydrea <ydrea.wrld@gmail.com>
2022-11-17 23:36:57 +0100

    ok

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,15 @@
 export default () => (
-    <footer>
-        <div>© 2020 - Alex The Entreprenerd - Development Sponsored by 
-        <a 
-            target="_blank" 
-            rel="noopener noreferrer" 
-            href="https://magic.link/"
-        > Magic </a>
-        </div>
-        <a href="https://www.youtube.com/watch?v=385cpCpGRC0&utm_source=site">
-            <h4>
-                Learn to code this on youtube <img className="img__yt" src="/NextJS.jpg" />
-            </h4>
-                
-        </a>
-    </footer>
-)
+  <footer>
+    <div>
+      © 2022 - Da
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/ydrea"
+      >
+        ydrea
+      </a>
+      m Nation
+    </div>
+  </footer>
+);
--- components/Header.js
+++ components/Header.js
@@ -28,7 +28,7 @@ export default () => {
       <div className={styles.title}>
         <Link href="/">
           <a>
-            <img src="/logo.png" alt="logo" />
+            <img src="/logo.svg" height="120" alt="logo" />
             <h3>WHITE TREE</h3>
           </a>
         </Link>
